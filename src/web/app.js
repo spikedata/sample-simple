67e59cb0c1190961420e18e470dbb2b52832fc97
ilan <ilan@spikedata.co.za>
2020-05-06 09:20:15 +0200

    run on single site

--- src/web/app.js
+++ src/web/app.js
@@ -1,5 +1,19 @@
 const config = require("./config");
 const scrapeAll = require("./scrapeAll");
-const T = require("./testAccounts");
+let T = require("./testAccounts");
+
+// run on single site e.g.
+//  - npm run start:web -- ned
+//  - node ./src/web/app.js ned
+if (process.argv.length === 3) {
+  let key = process.argv[2];
+  if (!T[key]) {
+    console.error("site not found:", key);
+    process.exit(-1);
+  }
+  T = {
+    [key]: T[key],
+  };
+}
 
 scrapeAll(T, config);
